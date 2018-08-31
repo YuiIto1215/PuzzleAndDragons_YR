32680b4dec441a2f2173f994f12a84cfa8b7d6f9
YuiIto1215 <aizu.s1240004@gmail.com>
2018-08-31 17:39:35 +0900

    test commit

--- PuzzleAndDragonsSoft-5/src/main/java/com/example/demo/PuzzleAndDragonsSoft5Application.java
+++ PuzzleAndDragonsSoft-5/src/main/java/com/example/demo/PuzzleAndDragonsSoft5Application.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PuzzleAndDragonsSoft5Application {
 
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(PuzzleAndDragonsSoft5Application.class, args);
 	}
